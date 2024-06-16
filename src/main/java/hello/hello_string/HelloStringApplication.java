2b741ee6a463d5d6f2767563a18ede080b59d841
optimal99 <stegano9744@gmail.com>
2024-06-16 11:47:45 +0900

    Initialize app

---
+++ src/main/java/hello/hello_string/HelloStringApplication.java
@@ -0,0 +1,13 @@
+package hello.hello_string;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hello_string/HelloStringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hello_string;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

78dea6691a14534071543582786c96d126eb0e00
optimal99 <stegano9744@gmail.com>
2024-07-09 13:45:02 +0900

    Feat: 섹션 2-2. MVC와 템플릿 엔진

--- src/main/java/hello/hello_string/controller/HelloController.java
+++ src/main/java/hello/hello_string/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_string.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +13,14 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Hello, Spring!");
         return "hello2";
+    }
+
+    // @RequestParam(value="name", required=true)) 형태가 default이다.
+    // required=false로 설정하면 name이라는 파라미터가 없어도 오류가 발생하지 않는다.
+    // http://localhost:8080/hello-mvc?name=Spring 형태로 데이터를 전달한다.
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
